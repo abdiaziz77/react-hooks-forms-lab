4420278d6a8f4f715321806e0e3e06f9f43c1abe
abdiaziz77 <abdiaziznoor20@gmail.com>
2025-04-26 17:42:34 +0300

    done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,28 +1,27 @@
 import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
-import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+function ShoppingList({ items, onItemFormSubmit }) {
+  const [searchText, setSearchText] = useState("");
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
+  function handleSearchChange(newSearchText) {
+    setSearchText(newSearchText);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
-  });
+  const itemsToDisplay = items.filter((item) =>
+    item.name.toLowerCase().includes(searchText.toLowerCase())
+  );
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter searchText={searchText} onSearchChange={handleSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+          <li key={item.id}>
+            {item.name} ({item.category})
+          </li>
         ))}
       </ul>
     </div>
